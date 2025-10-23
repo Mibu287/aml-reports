412762ff8ca6d4cd2d37636ed82854bb7b61b23a
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-23 16:34:35 +0700

    Refactor

---
+++ src/launch.rs
@@ -0,0 +1,33 @@
+use scopeguard::defer;
+
+pub(crate) async fn launch_automation_task<TaskFuture>(
+    func: fn(thirtyfour::WebDriver) -> TaskFuture,
+    port: u16,
+) -> anyhow::Result<()>
+where
+    TaskFuture: Future<Output = anyhow::Result<thirtyfour::WebDriver>>,
+{
+    // Create Chrome capabilities
+    let mut caps = thirtyfour::DesiredCapabilities::chrome();
+
+    // Launch chromedriver on the specified port
+    let mut chromedriver = thirtyfour_chromedriver::manager::Handler::new()
+        .launch_chromedriver(&mut caps, &port.to_string())
+        .await?;
+
+    defer!(
+        let _ = chromedriver.kill();
+    );
+
+    // Connect to chrome on the same port
+    let driver_url = format!("http://localhost:{}", port);
+    let driver = thirtyfour::WebDriver::new(&driver_url, caps).await?;
+
+    match func(driver).await {
+        Ok(driver) => {
+            driver.quit().await?;
+            Ok(())
+        }
+        Err(err) => Err(err),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use scopeguard::defer;
+mod launch;
 
-async fn main_task(driver: &thirtyfour::WebDriver) -> anyhow::Result<()> {
+use launch::launch_automation_task;
+
+async fn main_task(driver: thirtyfour::WebDriver) -> anyhow::Result<thirtyfour::WebDriver> {
     const BASE_URL: &str = "https://amlstr.sbv.gov.vn";
 
     // Go to website
@@ -20,32 +22,13 @@ async fn main_task(driver: &thirtyfour::WebDriver) -> anyhow::Result<()> {
     .await??;
 
     tokio::time::sleep(std::time::Duration::from_secs(500)).await;
-    Ok(())
+    Ok(driver)
 }
+
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // Create Chrome capabilities
     let port = 9515;
-    let mut caps = thirtyfour::DesiredCapabilities::chrome();
-
-    // Launch chromedriver on port 9515
-    let mut chromedriver = thirtyfour_chromedriver::manager::Handler::new()
-        .launch_chromedriver(&mut caps, &port.to_string())
-        .await?;
-
-    defer!(
-        let _ = chromedriver.kill();
-    );
-
-    // Connect to chrome on the same port
-    let driver = thirtyfour::WebDriver::new(&format!("http://localhost:{}", port), caps).await?;
-
-    let status = match main_task(&driver).await {
-        Ok(_) => Ok(()),
-        Err(err) => Err(err),
-    };
-
-    driver.quit().await?;
+    let status = launch_automation_task(main_task, port).await;
     status
 }
