a3d23d797c899356c6d712317e3a10d2a5cff17c
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-23 17:03:18 +0700

    Get cookies from current session

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use launch::launch_web_automation_task;
 
 async fn main_task(driver: thirtyfour::WebDriver) -> anyhow::Result<thirtyfour::WebDriver> {
     const BASE_URL: &str = "https://amlstr.sbv.gov.vn";
+    const SSO_URL: &str = "https://amlsso.sbv.gov.vn";
 
     // Go to website
     driver.goto(BASE_URL).await?;
@@ -20,6 +21,33 @@ async fn main_task(driver: thirtyfour::WebDriver) -> anyhow::Result<thirtyfour::
         Ok::<(), anyhow::Error>(())
     })
     .await??;
+
+    // Navigate to SSO URL to get cookies
+    driver.goto(SSO_URL).await?;
+    let cookies = tokio::time::timeout(std::time::Duration::from_secs(300), async {
+        loop {
+            let cookies = match driver.get_all_cookies().await {
+                Ok(cookies) => cookies,
+                Err(err) => {
+                    return Err(err);
+                }
+            };
+
+            for cookie in &cookies {
+                if cookie.name == "KC_RESTART" {
+                    return Ok(cookies);
+                }
+            }
+
+            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        }
+    })
+    .await??;
+
+    println!("Cookies: {:?}", cookies);
+
+    // Go back to dashboard
+    driver.goto(dashboard_url).await?;
 
     tokio::time::sleep(std::time::Duration::from_secs(500)).await;
     Ok(driver)
