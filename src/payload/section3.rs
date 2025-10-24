ec46a2ac0fb972ce5953bc1f61de9f71ccfc888c
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-24 16:24:14 +0700

    Add section 3

--- src/payload/section3.rs
+++ src/payload/section3.rs
@@ -1,4 +1,14 @@
+use crate::payload::entities::{Individual, Organization};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Default, Clone)]
-pub struct Section3 {}
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+pub struct Section3 {
+	#[serde(rename = "ca_nhan_lien_quan")]
+	pub related_individuals: Option<Vec<Individual>>,
+
+	#[serde(rename = "to_chuc_lien_quan")]
+	pub related_organizations: Option<Vec<Organization>>,
+
+	#[serde(rename = "thong_tin_khac_bo_sung")]
+	pub additional_info: Option<String>,
+}
