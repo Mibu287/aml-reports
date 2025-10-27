f20de7c77ee73ebba3de8c25f601fe2e41600b3e
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-27 14:31:48 +0700

    pretty print json data

--- src/bin/read_excel.rs
+++ src/bin/read_excel.rs
@@ -6,8 +6,9 @@ async fn main() -> anyhow::Result<()> {
     let path = "input/[2025.10.07] STR_Template.xlsx";
     let mut workbook: Xlsx<_> = open_workbook(path)?;
 
-    let form = payload::form::Form::from_excel(&mut workbook);
-    println!("{:#?}", form);
+    let form = payload::form::Form::from_excel(&mut workbook)?;
+    let json_form = serde_json::to_string_pretty(&form)?;
+    println!("{}", json_form);
 
     Ok(())
 }
