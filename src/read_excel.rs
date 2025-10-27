fbae182c75c0ae251c9281f5654c61c847fbcc1c
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-27 10:41:09 +0700

    print form to send to API

--- src/read_excel.rs
+++ src/read_excel.rs
@@ -1,3 +1,4 @@
+use aml::payload;
 use calamine::{DataType, Reader, Xlsx, open_workbook};
 
 #[tokio::main]
@@ -5,14 +6,8 @@ async fn main() -> anyhow::Result<()> {
     let path = "input/[2025.10.07] STR_Template.xlsx";
     let mut workbook: Xlsx<_> = open_workbook(path)?;
 
-    if let Ok(range) = workbook.worksheet_range("Sheet1") {
-        for row in range.rows().skip(1) {
-            // Skip header row
-            let name: String = row[0].get_string().unwrap_or_default().to_string();
-            let age: u32 = row[1].get_float().unwrap_or(0.0) as u32;
-            println!("Name: {}, Age: {}", name, age);
-        }
-    }
+    let form = payload::form::Form::from_excel(&mut workbook);
+    println!("{:#?}", form);
 
     Ok(())
 }
