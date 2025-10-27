52730ece17f488b34dc1f377e44cc6cee2a4d846
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-27 20:03:10 +0700

    Implement from_excel for Section 3

--- src/excel/section3.rs
+++ src/excel/section3.rs
@@ -3,10 +3,14 @@ use std::io::{Read, Seek};
 use crate::payload::section3::Section3;
 
 impl Section3 {
-    pub fn from_excel<RS>(workbook: &mut calamine::Xlsx<RS>) -> anyhow::Result<Self>
+    pub fn from_excel<RS>(_workbook: &mut calamine::Xlsx<RS>) -> anyhow::Result<Self>
     where
         RS: Seek + Read,
     {
-        Ok(Default::default())
+        Ok(Section3 {
+            related_individuals: None,
+            related_organizations: None,
+            additional_info: None,
+        })
     }
 }
