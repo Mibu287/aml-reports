348bc8ee05d1be9eee9fb4f7b4212baae392a132
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-29 13:52:37 +0700

    Add progress bar

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,6 @@
 use aml::{auth::get_auth_code, launch::launch_web_automation_task, payload::form::Form};
+use duration_extender::DurationExt;
+use indicatif::ProgressStyle;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,26 +21,61 @@ async fn main() -> anyhow::Result<()> {
 
     let api_url = "https://amlstr.sbv.gov.vn/strcreator/api/str-creator/saveStrModel?tabNo=0";
 
+    let progress_bar = indicatif::ProgressBar::new_spinner()
+        .with_message("Processing file...")
+        .with_style(ProgressStyle::with_template(
+            "[{elapsed_precise}] {bar:60.green/white} {pos:>7}/{len:7} {msg}",
+        )?);
+
+    progress_bar.set_length(excel_files.iter().len() as u64);
+
     for excel_file in excel_files {
         let mut workbook: calamine::Xlsx<_> = calamine::open_workbook(excel_file.path())?;
         let form_payload = Form::from_excel(&mut workbook)?;
 
-        let resp = reqwest::Client::new()
+        let status = reqwest::Client::new()
             .post(api_url)
             .bearer_auth(&auth_key_value)
             .json(&form_payload)
             .send()
-            .await?;
+            .await;
 
-        if !resp.status().is_success() {
-            eprintln!("Failed to submit form for file {:?}", excel_file.path());
-        } else {
-            println!(
-                "Successfully submitted form for file {:?}",
-                excel_file.path()
-            );
-        }
+        let _resp = match status {
+            Err(err) => {
+                progress_bar.println(format!(
+                    "Error submitting form for file {:?}: {}",
+                    excel_file.path(),
+                    err
+                ));
+                continue;
+            }
+            Ok(r) => match r.status().is_success() {
+                false => {
+                    progress_bar.println(format!(
+                        "Failed to submit form for file {:?}: HTTP {}",
+                        excel_file.path(),
+                        r.status()
+                    ));
+                    continue;
+                }
+                true => r,
+            },
+        };
+
+        progress_bar.println(format!(
+            "Successfully submitted form for file {:?}",
+            excel_file.path()
+        ));
+
+        tokio::time::sleep(500.milliseconds()).await;
+        progress_bar.inc(1);
+        progress_bar.set_message(format!(
+            "Processing file: {:?}",
+            excel_file.path().file_name().unwrap_or_default()
+        ));
     }
+
+    progress_bar.finish_with_message("DONE!!!");
 
     Ok(())
 }
