1fcc266b507848cd6f3029bf61e7760f7bfa8a35
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-29 14:44:07 +0700

    Fix read_excel.rs

--- src/bin/read_excel.rs
+++ src/bin/read_excel.rs
@@ -3,12 +3,25 @@ use calamine::{Xlsx, open_workbook};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let path = "input/(2025.10.27) STR_323.2025.xlsx";
-    let mut workbook: Xlsx<_> = open_workbook(path)?;
+    let excel_files = std::fs::read_dir("input")?
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.path().extension().and_then(|s| s.to_str()) == Some("xlsx"))
+        .filter(|entry| {
+            !entry
+                .file_name()
+                .to_str()
+                .unwrap_or_default()
+                .starts_with("~$")
+        })
+        .collect::<Vec<_>>();
 
-    let form = payload::form::Form::from_excel(&mut workbook)?;
-    let json_form = serde_json::to_string_pretty(&form)?;
-    println!("{}", json_form);
+    for excel_file in excel_files {
+        let excel_path = excel_file.path();
+        let mut workbook: Xlsx<_> = open_workbook(excel_path)?;
+        let form = payload::form::Form::from_excel(&mut workbook)?;
+        let json_form = serde_json::to_string_pretty(&form)?;
+        println!("{}", json_form);
+    }
 
     Ok(())
 }
