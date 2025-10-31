dd5379c820b17694dec3bc1d70dfeb286edff152
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-31 15:31:11 +0700

    Add section 6

--- src/excel/section6.rs
+++ src/excel/section6.rs
@@ -1,12 +1,45 @@
 use std::io::{Read, Seek};
 
-use crate::payload::section6::Section6;
+use crate::{
+    codes::document_type::DocumentType,
+    excel::section2::{get_cell_value, read_table_from_sheet},
+    payload::section6::{Attachment, Section6},
+};
 
 impl Section6 {
-    pub fn from_excel<RS>(_workbook: &mut calamine::Xlsx<RS>) -> anyhow::Result<Self>
+    pub fn from_excel<RS>(workbook: &mut calamine::Xlsx<RS>) -> anyhow::Result<Self>
     where
         RS: Seek + Read,
     {
-        Ok(Default::default())
+        let sheet_key = "Phần VI. Tài liệu đính kèm";
+
+        let (rows, col_map, base_coord) = read_table_from_sheet(workbook, sheet_key)?;
+
+        let attachments = rows
+            .into_iter()
+            .map(|curr_row| {
+                let cell_value_func =
+                    |col_name: &str| get_cell_value(col_name, &col_map, base_coord, &curr_row);
+
+                Attachment {
+                    str_id: None,
+                    status: "ACTIVE".to_string().into(),
+                    attachment_type: cell_value_func("Loại tài liệu").to_document_type().into(),
+                    page_count: cell_value_func("Số trang")
+                        .unwrap_or_default()
+                        .parse::<i32>()
+                        .ok()
+                        .into(),
+                    description: cell_value_func("Mô tả tài liệu").into(),
+                    file_name: cell_value_func("Tài liệu đính kèm (viết theo tên file đính kèm)")
+                        .into(),
+                    file_type: None,
+                    file_size: None,
+                    file: None,
+                }
+            })
+            .collect::<Vec<_>>();
+
+        Ok(Section6 { attachments })
     }
 }
--- src/payload/section6.rs
+++ src/payload/section6.rs
@@ -1,4 +1,26 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+pub struct Attachment {
+    #[serde(rename = "strId")]
+    pub str_id: Option<i64>,
+    pub status: Option<String>,
+    #[serde(rename = "attachmentType")]
+    pub attachment_type: Option<String>,
+    #[serde(rename = "pageCount")]
+    pub page_count: Option<i32>,
+    pub description: Option<String>,
+    #[serde(rename = "fileName")]
+    pub file_name: Option<String>,
+    #[serde(rename = "fileType")]
+    pub file_type: Option<String>,
+    #[serde(rename = "fileSize")]
+    pub file_size: Option<i64>,
+    pub file: Option<()>,
+}
+
 #[derive(Debug, Serialize, Deserialize, Default, Clone)]
-pub struct Section6 {}
+pub struct Section6 {
+    #[serde(flatten)]
+    pub attachments: Vec<Attachment>,
+}
