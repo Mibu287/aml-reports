142da157f8224c56fc6ab094b150c0a017c7dfeb
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-11-01 00:20:58 +0700

    Fix read_excel.rs

--- src/bin/read_excel.rs
+++ src/bin/read_excel.rs
@@ -1,27 +1,37 @@
-use aml::payload;
+use aml::{
+    payload,
+    utils::setup::{get_input_excel_files, initial_setup},
+};
+use anyhow::Context;
 use calamine::{Xlsx, open_workbook};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let excel_files = std::fs::read_dir("input")?
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.path().extension().and_then(|s| s.to_str()) == Some("xlsx"))
-        .filter(|entry| {
-            !entry
-                .file_name()
-                .to_str()
-                .unwrap_or_default()
-                .starts_with("~$")
-        })
-        .collect::<Vec<_>>();
+    let progress_bar = initial_setup()?;
+    let excel_files = get_input_excel_files()?;
+    progress_bar.set_length(excel_files.len() as u64);
 
     for excel_file in excel_files {
         let excel_path = excel_file.path();
-        let mut workbook: Xlsx<_> = open_workbook(excel_path)?;
-        let form = payload::form::Form::from_excel(&mut workbook)?;
-        let json_form = serde_json::to_string_pretty(&form)?;
+
+        let mut workbook: Xlsx<_> = open_workbook(excel_path.clone())
+            .with_context(|| format!("Không thể mở file {:#?}", excel_path))?;
+
+        let form = payload::form::Form::from_excel(&mut workbook)
+            .with_context(|| format!("Lỗi khi đọc và xử lý dữ liệu từ file {:#?}", excel_path))?;
+
+        let json_form = serde_json::to_string_pretty(&form).with_context(|| {
+            format!(
+                "Lỗi khi chuyển đổi dữ liệu thành file {:#?} thành định dạng JSON",
+                excel_path
+            )
+        })?;
+
         println!("{}", json_form);
+        progress_bar.inc(1);
     }
+
+    progress_bar.finish_with_message("DONE!!!");
 
     Ok(())
 }
