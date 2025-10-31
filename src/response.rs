3dfb0e174ef816aa9f39d622c40da24b3b947579
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-10-31 12:53:21 +0700

    Add struct SuccessResponse

--- src/response.rs
+++ src/response.rs
@@ -1,7 +1,15 @@
 use serde::{Deserialize, Serialize};
 
+use crate::payload::form::Form;
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ErrorResponse {
     pub status: u32,
     pub message: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SuccessResponse {
+    #[serde(flatten)]
+    pub form: Form,
 }
