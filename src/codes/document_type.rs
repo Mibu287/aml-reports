9277f82c25db5bfa95d620dc426e3e16ad39e251
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-11-03 17:30:51 +0700

    Process attachments

--- src/codes/document_type.rs
+++ src/codes/document_type.rs
@@ -1,6 +1,4 @@
-use crate::codes::utils::search_for_code;
-
-const DOCUMENT_TYPES: [(&str, &str); 7] = [
+const DOCUMENT_TYPES: [(&'static str, &'static str); 7] = [
     ("STM", "Bảng kê"),
     ("FLW", "Minh họa dòng tiền"),
     ("REL", "Minh họa mối quan hệ khách hàng"),
@@ -19,14 +17,27 @@ impl DocumentType for String {
         match self.as_str() {
             "" => Ok(String::new()),
             _ => {
-                let code = search_for_code(&DOCUMENT_TYPES, self);
-                if code.is_empty() {
+                let check = DOCUMENT_TYPES
+                    .into_iter()
+                    .map(|d| d.0)
+                    .filter(|code| *code == self.as_str())
+                    .collect::<Vec<_>>();
+
+                if check.len() == 0 {
                     Err(anyhow::anyhow!(
-                        "Loại tài liệu đính kèm không hợp lệ: {}",
-                        self
+                        "Loại tài liệu đính kèm không hợp lệ: {}. Danh sách loại tài liệu hợp lệ như sau: {}",
+                        self,
+                        DOCUMENT_TYPES
+                            .into_iter()
+                            .fold(String::new(), |result, (code, name)| {
+                                match result.as_str() {
+                                    "" => format!("{}: {}", code, name),
+                                    _ => format!("{}, {}: {}", result, code, name),
+                                }
+                            })
                     ))
                 } else {
-                    Ok(code)
+                    Ok(self.clone())
                 }
             }
         }
--- src/excel/section6.rs
+++ src/excel/section6.rs
@@ -1,4 +1,7 @@
-use std::io::{Read, Seek};
+use std::{
+    io::{Read, Seek},
+    path::PathBuf,
+};
 
 use anyhow::Context;
 
@@ -11,97 +14,118 @@ use crate::{
 impl Section6 {
     pub fn from_excel<RS>(
         workbook: &mut calamine::Xlsx<RS>,
-        _file_path: &std::path::Path,
+        file_path: &std::path::Path,
     ) -> anyhow::Result<Self>
     where
         RS: Seek + Read,
     {
-        Self::_from_excel(workbook)
+        Self::_from_excel(workbook, file_path)
             .with_context(|| format!("Lỗi xử lý dữ liệu Phần VI - Tài liệu đính kèm"))
     }
 
-    fn _from_excel<RS>(workbook: &mut calamine::Xlsx<RS>) -> anyhow::Result<Self>
+    fn _from_excel<RS>(
+        _workbook: &mut calamine::Xlsx<RS>,
+        file_path: &std::path::Path,
+    ) -> anyhow::Result<Self>
     where
         RS: Seek + Read,
     {
-        let sheet_key = "Phần VI. Tài liệu đính kèm";
+        let file_name = file_path
+            .file_stem()
+            .map(|name| name.to_string_lossy().to_string())
+            .unwrap_or_default();
 
-        let (rows, col_map, base_coord) = read_table_from_sheet(workbook, sheet_key)?;
+        let attachment_folder = {
+            let mut folder = PathBuf::new();
+            folder.push("input");
+            folder.push(file_name);
+            folder
+        };
+        if !attachment_folder.exists() || !attachment_folder.is_dir() {
+            return Err(anyhow::anyhow!(
+                "Không tìm thấy folder chứa các file đính kèm {:#?}. Bổ sung thêm folder đính kèm và đặt tên folder trùng với tên file.",
+                attachment_folder.as_path()
+            ));
+        }
 
-        let attachments = rows
-            .into_iter()
-            .map(|curr_row| -> anyhow::Result<Attachment> {
-                let cell_value_func =
-                    |col_name: &str| get_cell_value(col_name, &col_map, base_coord, &curr_row);
+        let file_list = std::fs::read_dir(&attachment_folder)
+            .with_context(|| format!("Không thể mở folder {:#?}", &attachment_folder))?;
 
-                let file_path: std::path::PathBuf =
-                    cell_value_func("Tài liệu đính kèm (viết theo tên file đính kèm)")?
-                        .unwrap_or_default()
-                        .into();
+        let mut attachments = vec![];
 
-                if !file_path.exists() || !file_path.is_file() {
-                    return Err(anyhow::anyhow!(
-                        "Đường dẫn tài liệu đính kèm không tồn tại: {:?}",
-                        file_path
-                    ));
-                }
+        for file in file_list {
+            let file =
+                file.with_context(|| format!("Không thể mở folder {:#?}", &attachment_folder))?;
 
-                let file_name = file_path
-                    .file_name()
-                    .map(|s| s.to_string_lossy().to_string());
+            let file_name = file
+                .path()
+                .file_stem()
+                .unwrap_or_default()
+                .to_string_lossy()
+                .to_string();
 
-                let file_ext = file_path
-                    .extension()
-                    .map(|s| s.to_string_lossy().to_string());
+            if file_name.starts_with("~$") {
+                continue;
+            }
 
-                let file_mime = mime_guess::from_path(&file_path)
-                    .first_or_octet_stream()
-                    .to_string()
-                    .into();
-
-                let file_content = std::fs::read(&file_path).with_context(|| {
-                    format!("Lỗi không đọc được nội dung file đính kèm: {:?}", file_path)
+            let file_prefix = file_name
+                .split("_")
+                .next()
+                .unwrap_or_default()
+                .to_string()
+                .to_uppercase()
+                .to_document_type()
+                .with_context(|| {
+                    format!(
+                        "File đính kèm {:#?} có tiền tố không hợp lệ",
+                        file.path().as_path()
+                    )
                 })?;
 
-                let file_size = file_content.len() as i64;
+            let file_desc = file_name
+                .strip_prefix(&file_prefix)
+                .unwrap_or_default()
+                .to_string();
 
-                let attachment = Attachment {
-                    str_id: None,
-                    status: "ACTIVE".to_string().into(),
-                    attachment_type: cell_value_func("Loại tài liệu")?.to_document_type()?.into(),
-                    page_count: cell_value_func("Số trang")?
-                        .unwrap_or_default()
-                        .parse::<i32>()
-                        .map_err(|_| anyhow::anyhow!("Thông tin số trang không hợp lệ"))?
-                        .into(),
-                    description: cell_value_func("Mô tả tài liệu")?.into(),
-                    file_name: file_name,
-                    file_type: file_ext,
-                    file_size: file_size.into(),
-                    file: Default::default(),
-                    file_mime: file_mime,
-                    file_content: file_content.into(),
-                };
-                Ok(attachment)
-            })
-            .enumerate()
-            .fold(
-                anyhow::Result::<Vec<Attachment>>::Ok(Vec::new()),
-                |final_result, element| {
-                    let mut final_result = final_result?;
+            let file_ext = file
+                .path()
+                .extension()
+                .map(|s| s.to_string_lossy().to_string())
+                .unwrap_or_default();
 
-                    let (n_row, current_result) = element;
-                    let err_context = || {
-                        format!(
-                            "Lỗi dữ liệu khi xử lý dòng số {}",
-                            n_row + base_coord.0 as usize + 2
-                        )
-                    };
-                    let current_result = current_result.with_context(err_context)?;
-                    final_result.push(current_result);
-                    Ok(final_result)
-                },
-            )?;
+            let page_count = match file_ext.to_lowercase().as_str() {
+                "pdf" => 0,
+                _ => 1,
+            };
+
+            let file_mime = mime_guess::from_path(file.path())
+                .first_or_octet_stream()
+                .to_string()
+                .into();
+
+            let file_content = std::fs::read(file.path())
+                .with_context(|| format!("Không thể đọc file {}", file.path().to_string_lossy()))?;
+
+            let file_size = file_content.len() as i64;
+
+            attachments.push(Attachment {
+                str_id: None,
+                status: "ACTIVE".to_string().into(),
+                attachment_type: file_prefix.into(),
+                page_count: page_count.into(),
+                description: file_desc.into(),
+                file_name: file
+                    .path()
+                    .file_stem()
+                    .map(|stem| stem.to_string_lossy().to_string())
+                    .into(),
+                file_type: file_ext.into(),
+                file_size: file_size.into(),
+                file: Default::default(),
+                file_mime: file_mime,
+                file_content: file_content.into(),
+            });
+        }
 
         Ok(Section6 { attachments })
     }
