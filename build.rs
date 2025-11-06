ec326b7668fbe1d9b12de8e269c5e234610546bc
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-11-06 15:49:57 +0700

    Do no compile with feature 'packaging' when git repo is not clean

--- build.rs
+++ build.rs
@@ -1,6 +1,13 @@
 use shadow_rs::{BuildPattern, ShadowBuilder};
 
 fn main() {
+    #[cfg(feature = "packaging")]
+    {
+        if !shadow_rs::git_clean() {
+            panic!("Can not build with feature 'packaging' when git repo is not clean!!!")
+        }
+    }
+
     ShadowBuilder::builder()
         .build_pattern(BuildPattern::RealTime)
         .build()
