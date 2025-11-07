0bba91d7c07a7cd64c8e7a1cf08044f67939cbe8
BUI DUC MINH(013270) <minhbd.ho@vietcombank.com.vn>
2025-11-07 11:55:13 +0700

    Fix datetime check

--- src/utils/datetime.rs
+++ src/utils/datetime.rs
@@ -1,3 +1,5 @@
+use chrono::Datelike;
+
 pub trait ConvertDateFormat {
     fn convert_date_format(
         &self,
@@ -25,11 +27,17 @@ impl ConvertDateFormat for &str {
         let from_format = "%d/%m/%Y";
         let to_format = "%Y-%m-%d";
 
-        let formatted_date = chrono::NaiveDate::parse_from_str(self.trim().as_ref(), from_format)
-            .map_err(|_| anyhow::anyhow!("Ngày '{}' không theo định dạng 'dd/mm/yyyy'", self))?
-            .format(to_format)
-            .to_string();
+        let date = chrono::NaiveDate::parse_from_str(self.trim().as_ref(), from_format)
+            .map_err(|_| anyhow::anyhow!("Ngày '{}' không theo định dạng 'dd/mm/yyyy'", self))?;
 
+        if date.year() < 1900 || date.year() > 2099 {
+            return Err(anyhow::anyhow!(
+                "Ngày {} nằm ngoài khoảng thời gian hợp lý (01/01/1900 -> 31/12/2099)",
+                self
+            ));
+        }
+
+        let formatted_date = date.format(to_format).to_string();
         Ok(Some(formatted_date))
     }
 }
